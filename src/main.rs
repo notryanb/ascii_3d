8b92d75fa663165c832f13610bb2fad8a478f8be
notryanb@gmail.com <notryanb@gmail.com>
2019-03-06 21:18:26 -0500

    got first stationary 3d perspective

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
-use pancurses::{initscr, noecho};
+use pancurses::*;
 
 use std::f32::consts::PI;
-use std::f32::*;
+use std::time::SystemTime;
 
 fn main() {
     // let screen_width = 120;
     // let screen_height = 40;
     let window = initscr();
-    window.nodelay(true);
+    nl();
     noecho();
+    window.nodelay(true);
+    
+    let screen_width = window.get_max_x();
+    let screen_height = window.get_max_y();
 
     let player_x = 14.7; // Player start position
     let player_y = 5.09;
     let player_a = 0.0; // player start rotation
 
-    let screen_width = window.get_max_x();
-    let screen_height = window.get_max_y();
     let map_width = 16;
     let map_height = 16;
     let field_of_view = PI / 4.0;
@@ -23,31 +25,53 @@ fn main() {
     let depth = 16.0;
     let speed = 5.0;
 
-    let screen_buffer: Vec<&str> = Vec::with_capacity((screen_width * screen_height) as usize);
+    let mut screen_buffer: Vec<char> = Vec::with_capacity((screen_width * screen_height) as usize);
 
-    let mut map = "".to_string();
-    map += "################";
-    map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
-    map += "#..............#";
-    map += "################";
+    for i in 0..screen_width * screen_height {
+        screen_buffer.push(' ');
+    }
 
+    let mut string_map = "".to_string();
+    string_map += "################";
+    string_map += "#..............#";
+    string_map += "#..............#";
+    string_map += "#..............#";
+    string_map += "#..............#";
+    string_map += "#..............#";
+    string_map += "#..............#";
+    string_map += "#..............#";
+    string_map += "#..............#";
+    string_map += "#..............#";
+    string_map += "#..............#";
+    string_map += "#..............#";
+    string_map += "#..............#";
+    string_map += "#..............#";
+    string_map += "#..............#";
+    string_map += "################";
 
-    while window.getch().is_none() {
-        // Handle Player rotation
-        // Clockwise
-        // Counter Clockwise
+    let mut map: Vec<_> = string_map.chars().collect();
+    let mut time_1 = SystemTime::now();
+
+    'gameloop: loop {
+        let _elapsed = time_1.elapsed().unwrap();
+        time_1 = SystemTime::now();
+        
+
+        match window.getch() {
+            Some(Input::Character(q)) if q == 'q' => {
+                curs_set(1);
+                endwin();
+                return;
+            }
+            Some(Input::Character(a)) if a == 'a' => {
+                // player_a -= elapsed.checked_mul(speed * 0.75);
+            }
+            Some(Input::Character(d)) if d == 'd' => {
+                // player_a += elapsed.checked_mul(speed * 0.75);
+            }
+            _ => {}
+        }
+
         // Forwards && Collision
         // Backwards && Collision
 
@@ -61,14 +85,14 @@ fn main() {
             let mut distance_to_wall = 0.0;
 
             let mut hit_wall = false;
-            let boundary = false;
+            let _boundary = false;
 
             let eye_x = ray_angle.sin();
             let eye_y = ray_angle.cos();
 
             // Cast ray from player along ray angle and test for intersection with block
             while !(hit_wall && (distance_to_wall < depth)) {
-                distance_to_wall += step_size; 
+                distance_to_wall += step_size;
 
                 let test_x = (player_x + eye_x * distance_to_wall) as i32;
                 let test_y = (player_y + eye_y * distance_to_wall) as i32;
@@ -80,24 +104,24 @@ fn main() {
                 }
                 else {
                     // Ray is in-bounds so test to see if the ray cell is a wall block
-                    if map[..][(test_x * map_width + test_y) as usize] == "#" {
+                    if map[(test_x * map_width + test_y) as usize] == '#' {
 
                         hit_wall = true;
-                        let corner_boundaries: Vec<(f32, f32)> = Vec::new();
+                        // let mut corner_boundaries: Vec<(f32, f32)> = Vec::new();
 
-                        for tx in 0..2 {
-                            for ty in 0..2 {
-                                let vy = test_y as f32 + ty as f32- player_y;
-                                let vx = test_x as f32 + tx as f32 - player_x;
-                                let d = (vx * vx + vy * vy).sqrt();
-                                let dot = (eye_x * vx / d) + (eye_y * vy / d);
-                                corner_boundaries.push((d, dot));
-                            }
-                        }
+                        // for tx in 0..2 {
+                        //     for ty in 0..2 {
+                        //         let vy = test_y as f32 + ty as f32- player_y;
+                        //         let vx = test_x as f32 + tx as f32 - player_x;
+                        //         let d = (vx * vx + vy * vy).sqrt();
+                        //         let dot = (eye_x * vx / d) + (eye_y * vy / d);
+                        //         corner_boundaries.push((d, dot));
+                        //     }
+                        // }
 
                         // Sort the pairs from closest to furthest.
 
-                    
+
                     }
                 }
             }
@@ -106,9 +130,24 @@ fn main() {
             let ceiling = (screen_height as f32 / 2.0) - screen_height as f32 / distance_to_wall as f32;
             let floor = screen_height as f32 - ceiling;
 
-            // Shader walls based on distance
+            // Shade walls based on distance
+            // Share the floor based on distance
+            
+            for y in 0..screen_height {
+                if y as f32 <= ceiling {
+                    screen_buffer[(y * screen_width + x) as usize] = ' ';
+                }
+                else if y as f32 > ceiling && y as f32 <= floor {
+                    screen_buffer[(y * screen_width + x) as usize] = 'x';
+                }
+                else {
+                    screen_buffer[(y * screen_width + x) as usize] = ' ';
+                }
 
-
+            }
         }
+
+        window.mvaddstr(0,0, screen_buffer.iter().collect::<String>());
+        window.refresh();
     }
 }
